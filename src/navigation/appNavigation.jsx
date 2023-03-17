5840410adcacd1e84c4d7d6e0622c8cb10890f0a
samina203 <saminabashir203@gmail.com>
2023-03-17 14:55:59 +0500

    Add Appnavigator

---
+++ src/navigation/appNavigation.jsx
@@ -0,0 +1,20 @@
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Login } from "../screens/login";
+
+function AppNavigator() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+export { AppNavigator };
---
+++ src/screens/login.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+function Login() {
+  return (
+    <View style={{ backgroundColor: "cyan", flex: 1 }}>
+      <Text>hhh</Text>
+    </View>
+  );
+}
+export { Login };
